4577542fc1564f8f5367363f69812f2be7ee7bd1
ahtcgd <135237380+ahtcgd@users.noreply.github.com>
2025-11-06 17:44:49 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -130,9 +130,8 @@ def run(playwright: Playwright) -> None:
               print("🍪 Cookies 已添加到浏览器上下文。")
 
             # 2. 访问验证页面
-            page.goto(VERIFY_URL, wait_until='domcontentloaded')
-            # 慢
-            # page.goto(VERIFY_URL, wait_until='networkidle')
+            # page.goto(VERIFY_URL, wait_until='domcontentloaded')
+            page.goto(VERIFY_URL, wait_until='networkidle')
             print(f"尝试访问验证 URL: {VERIFY_URL}")
 
             # 3. 确定用于判断成功的 Locator
